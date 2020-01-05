0c4e5addbee4fbc0b1db740f92e3e8cc2540bc73
JoseP-UR <urivesrosa@gmmail.com>
2020-01-05 17:54:48 -0300

    main page session, and logout

--- src/store/list/state.js
+++ src/store/list/state.js
@@ -3,10 +3,14 @@ export default {
         {
             id: 1,
             description: 'this is a description',
+            dateCreated: '01/01/2000',
+            dueDate: '22/11/2002'
         },
         {
             id: 2,
             description: 'this is a description too',
+            dateCreated: '01/01/2000',
+            dueDate: '22/11/2002'
         }
     ]
 }
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,19 +1,27 @@
 <template>
     <div class="Main">
-      <h1 class="-title _reduced">Welcome to your main page.</h1>
-      <control-panel>
-        <control-item :icon="['fas', 'plus']"  iconColor="#339900">
-            New
-        </control-item>
-      </control-panel>
-      <task-list></task-list>  
+        <h1 class="-title _reduced">Welcome to your main page, {{user.name}}.</h1>
+        
+        <div class="main-section">
+            <div class="controls">
+                <a class="-button" @click="logout()">Logout</a>
+            </div>
+
+            <div class="task-list">
+                <div class="task" v-for="task in tasks" :key="task.id">
+                    <div class="header">
+                        <span class="date"><b>Created: </b>{{task.dateCreated}}</span>
+                        <span class="date"><b>Due: </b>{{task.dueDate}}</span>
+                    </div>
+                    <div class="body" v-html="task.description"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import taskList from '../components/task-list'
-import controlPanel from '../components/control-panel'
-import controlItem from '../components/control-item'
+import { mapState } from 'vuex';
 
 export default {
     name: 'Main',
@@ -22,10 +30,38 @@ export default {
 
         }
     },
+    computed: {
+        ...mapState({
+            tasks: state => state.list.tasks,
+            user: state => state.user.data,
+        })
+    },
+    methods: {
+        login() {
+            if (this.$session.has('name') && this.$session.has('pass')) {
+            this.$store.dispatch('user/login', { name: this.$session.get('name'), pass: this.$session.get('pass'), session: true }).then(res => {
+                if (res.data.error) {
+                    console.error('corrupt session');
+                    this.$router.push('/');
+                    return;
+                }
+                this.$store.commit('user/SET_USER_DATA', res.data);
+            })
+        }
+        
+        },
+        logout() {
+            this.$session.clear();
+            this.$session.destroy();
+            this.$session.start();
+            this.$router.push('/');
+
+        }
+    },
+    mounted() {
+        this.login();
+    },
     components: {
-        taskList,
-        controlPanel,
-        controlItem
     }
 
 }
@@ -37,4 +73,6 @@ export default {
 
         >.-title
             margin: 0 auto 10px auto
+    
+
 </style>
